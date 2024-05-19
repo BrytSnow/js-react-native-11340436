69cc372d0e60136975dcc8cbe6cb45e83818ff0d
@fanboi <bganti@st.ug.edu.gh>
2024-05-19 07:58:47 +0000

    uploading arrayManipulation.js file

---
+++ arrayManipulation.js
@@ -0,0 +1,21 @@
+//Task 1
+function processArray(arr) {
+  return arr.map((num) => {
+    if (num % 2 === 0) {
+      return num * num;
+    } else {
+      return num * num * num;
+    }
+  });
+}
+
+//Task 2
+function formatArrayStrings(stringArray, numberArray) {
+  return stringArray.map((str, index) => {
+    if (numberArray[index] % 2 === 0) {
+      return str.toUpperCase();
+    } else {
+      return str.toLowerCase();
+    }
+  });
+}
