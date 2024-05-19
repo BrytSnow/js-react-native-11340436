c92e2cf7383330abae972d7e2737faa1d1befe2a
@fanboi <bganti@st.ug.edu.gh>
2024-05-19 07:59:55 +0000

    uploading userInfo.js file

---
+++ userInfo.js
@@ -0,0 +1,9 @@
+function createUserProfiles(nameArray, modifiedNameArray) {
+  return nameArray.map((name, index) => {
+    return {
+      originalName: name,
+      modifiedName: modifiedNameArray[index],
+      id: index + 1
+    };
+  });
+}
